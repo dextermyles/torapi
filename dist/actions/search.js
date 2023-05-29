9ce0e933d992f8bf514950eaef59c67b564c836e
Dexter Brock <dexter.brock@meridiancu.ca>
2023-05-28 22:13:50 -0400

    refactor

--- dist/actions/search.js
+++ dist/actions/search.js
@@ -5,18 +5,32 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.searchTorrentApi = void 0;
 const chalk_1 = __importDefault(require("chalk"));
+const lodash_1 = __importDefault(require("lodash"));
 const torrent_search_api_1 = __importDefault(require("torrent-search-api"));
 const search_result_1 = require("../models/search-result");
 function searchTorrentApi(keyword) {
     torrent_search_api_1.default.enablePublicProviders();
     const search = torrent_search_api_1.default.search(['ThePirateBay'], keyword, 'Video', 10);
+    let trackers = [];
     const promise = new Promise((resolve, reject) => {
         search.then((results) => {
             const torrents = results;
             const searchResults = torrents.map((t) => {
+                let trackersArr = t.magnet.split('&tr=');
+                if (trackersArr.length > 0) {
+                    trackersArr.forEach((element, i) => {
+                        if (i > 0) {
+                            let test = decodeURIComponent(element);
+                            let uri = decodeURI(element);
+                            trackers.push(test);
+                        }
+                    });
+                }
+                trackers = lodash_1.default.uniq(trackers);
                 const torr = new search_result_1.SearchResult(t.title, t.magnet, t.size, t.peers, t.seeds);
                 return torr;
             }).filter(x => x.seeds && x.seeds > 0);
+            console.dir(trackers);
             resolve(searchResults);
         }, (err) => {
             console.log(chalk_1.default.red(err));
--- src/actions/search.ts
+++ src/actions/search.ts
@@ -1,4 +1,5 @@
 import chalk from 'chalk';
+import _ from 'lodash';
 import TorrentSearchApi from 'torrent-search-api';
 import { SearchResult } from '../models/search-result';
 import { Torrent } from '../models/torrent';
@@ -7,14 +8,28 @@ export function searchTorrentApi(keyword: string): Promise<void> {
     TorrentSearchApi.enablePublicProviders();
     
     const search = TorrentSearchApi.search(['ThePirateBay'], keyword, 'Video', 10);
-    
+    let trackers: string[] = [];
+
     const promise = new Promise<SearchResult[]>((resolve, reject) => {
         search.then((results) => {
             const torrents = results as Torrent[];
             const searchResults = torrents.map((t) => {
+                let trackersArr = t.magnet.split('&tr=');
+                
+                if (trackersArr.length > 0) {
+                    trackersArr.forEach((element, i) => {
+                        if (i > 0) {
+                            let test = decodeURIComponent(element);
+                            let uri = decodeURI(element);
+                            trackers.push(test);
+                        }
+                    });
+                }
+                trackers = _.uniq(trackers);
                 const torr = new SearchResult(t.title, t.magnet, t.size, t.peers, t.seeds);
                 return torr;
             }).filter(x => x.seeds && x.seeds > 0);
+            console.dir(trackers);
             resolve(searchResults);
         }, (err) => {
             console.log(chalk.red(err));
